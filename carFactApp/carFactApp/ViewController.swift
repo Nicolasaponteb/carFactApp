d6dab9e779f785b3377e8e5b0f29bf8b50209875
Nicolas Aponte <nicolas.aponteb1@gmail.com>
2022-02-21 16:50:20 -0500

    Creación del proyecto

---
+++ carFactApp/carFactApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  carFactApp
+//
+//  Created by Nicolas Aponte on 2/21/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
